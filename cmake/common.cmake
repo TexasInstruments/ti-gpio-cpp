5ec0312ad540ab6be306fce77c2e23ee134271a3
Vijay Pothukuchi <vijayp@ti.com>
2022-07-11 20:41:44 -0500

    Added support for cross-compilation.
    Signed-off-by: Vijay Pothukuchi <vijayp@ti.com>

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -18,7 +18,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-cmake_minimum_required(VERSION 3.16.0)
+cmake_minimum_required(VERSION 3.10.2)
 
 include(GNUInstallDirs)
 include(CMakePackageConfigHelpers)
@@ -26,9 +26,7 @@ include(CMakePackageConfigHelpers)
 add_compile_options(-std=c++17)
 
 # Get the path of this file
-get_filename_component(SELF_DIR    ${CMAKE_CURRENT_LIST_FILE} PATH)
-get_filename_component(ROOT_DIR    ${SELF_DIR}/.. ABSOLUTE)
-get_filename_component(INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/.. ABSOLUTE)
+get_filename_component(ROOT_DIR    ${CMAKE_SOURCE_DIR} ABSOLUTE)
 
 IF(NOT CMAKE_BUILD_TYPE)
   SET(CMAKE_BUILD_TYPE Release)
@@ -48,95 +46,82 @@ set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/bin/${CMAKE_BUILD_TYPE})
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/bin/${CMAKE_BUILD_TYPE})
 
 # Include directories
-include_directories(${PROJECT_SOURCE_DIR}
-                    ${ROOT_DIR}/include)
+include_directories(${PROJECT_SOURCE_DIR} ${ROOT_DIR}/include)
 
-set(COMMON_LINK_LIBS
-    ti_gpio
-    )
+set(COMMON_LINK_LIBS ti_gpio)
 
-set(SYSTEM_LINK_LIBS
-    pthread
-    )
+set(SYSTEM_LINK_LIBS pthread)
 
-# Function for building an application
-# ARG0: app name
-# ARG1: source list
-function(build_app)
-    set(app ${ARGV0})
-    set(src ${ARGV1})
-    add_executable(${app} ${${src}})
-    target_link_libraries(${app}
-                          -Wl,--start-group
+# Function for building a application:
+# app_name: app name
+# ${ARGN} expands everything after the last named argument to the end
+# usage: build_app(app_name a.c b.c....)
+function(build_app app_name)
+    add_executable(${app_name} ${ARGN})
+    target_link_libraries(${app_name}
                           ${COMMON_LINK_LIBS}
+                          ${TARGET_LINK_LIBS}
                           ${SYSTEM_LINK_LIBS}
-                          -Wl,--end-group)
-endfunction(build_app)
+                         )
+endfunction()
 
 # Function for building a node:
-# ARG0: lib name
-# ARG1: source list
-# ARG2: type (STATIC, SHARED)
-function(build_lib)
-    set(lib ${ARGV0})
-    set(src ${ARGV1})
-    set(type ${ARGV2})
-    set(version 1.0.0)
-
-    add_library(${lib} ${type} ${${src}})
+# lib_name: Name of the library
+# lib_type: (STATIC, SHARED)
+# lib_ver: Version string of the library
+# sub_dir: optional directory under include and lib directories to place the content
+# ${ARGN} expands everything after the last named argument to the end
+# usage: build_lib(lib_name lib_type lib_ver a.c b.c ....)
+function(build_lib lib_name lib_type lib_ver)
+    add_library(${lib_name} ${lib_type} ${ARGN})
 
     set(INCLUDE_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR})
 
     message("INCLUDE_INSTALL_DIR = " ${INCLUDE_INSTALL_DIR})
-    install(TARGETS ${lib}
-            EXPORT ${lib}Targets
+    install(TARGETS ${lib_name}
+            EXPORT ${lib_name}Targets
             LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}  # Shared Libs
             ARCHIVE DESTINATION ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}  # Static Libs
             RUNTIME DESTINATION ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}  # Executables, DLLs
-            INCLUDES DESTINATION ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}
+            INCLUDES DESTINATION ${INCLUDE_INSTALL_DIR}
     )
 
-    FILE(GLOB HDRS
-        include/*.h
-    )
+    FILE(GLOB HDRS ${CMAKE_CURRENT_SOURCE_DIR}/include/*.h)
 
     # Specify the header files to install
-    install(FILES
-        ${HDRS}
-        DESTINATION ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}
-    )
+    install(FILES ${HDRS} DESTINATION ${INCLUDE_INSTALL_DIR})
 
-    set(CONFIG_PACKAGE_LOCATION ${CMAKE_INSTALL_PREFIX}/cmake/${PROJECT_NAME})
+    set(CONFIG_PACKAGE_LOCATION ${CMAKE_INSTALL_PREFIX}/cmake/${lib_name})
 
-    install(EXPORT ${PROJECT_NAME}Targets
-        FILE ${PROJECT_NAME}Targets.cmake
-        DESTINATION ${CONFIG_PACKAGE_LOCATION}
-    )
-
-    export(EXPORT ${PROJECT_NAME}Targets
-        FILE ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Targets.cmake
-    )
-
-    # Generate the version file for the config file
-    write_basic_package_version_file(
-        ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
-        VERSION "${version}"
-        COMPATIBILITY AnyNewerVersion
-    )
-    
     # Create config file
     configure_package_config_file(${ROOT_DIR}/cmake/ti_gpio_config.cmake.in
-        ${CMAKE_CURRENT_BINARY_DIR}/${lib}Config.cmake
+        ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}Config.cmake
         INSTALL_DESTINATION ${CONFIG_PACKAGE_LOCATION}
         PATH_VARS INCLUDE_INSTALL_DIR
     )
 
+    install(EXPORT ${lib_name}Targets
+        FILE ${lib_name}Targets.cmake
+        DESTINATION ${CONFIG_PACKAGE_LOCATION}
+    )
+
+    export(EXPORT ${lib_name}Targets
+        FILE ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}Targets.cmake
+    )
+
+    # Generate the version file for the config file
+    write_basic_package_version_file(
+        ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}ConfigVersion.cmake
+        VERSION "${lib_ver}"
+        COMPATIBILITY AnyNewerVersion
+    )
+    
     # install the package configuration files
     install(FILES 
-      ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
-      ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
+      ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}Config.cmake
+      ${CMAKE_CURRENT_BINARY_DIR}/${lib_name}ConfigVersion.cmake
       DESTINATION ${CONFIG_PACKAGE_LOCATION}
     )
 
-endfunction(build_lib)
+endfunction()
 
