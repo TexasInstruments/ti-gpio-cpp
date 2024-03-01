5a788ef140f1e6325ba7fc557a7d4b1aa7730381
Shreyash Sinha <s-sinha@ti.com>
2024-03-01 18:09:48 +0530

    [libgpiod] simple_out test working with libgpiod libraries

--- samples/simple_out.cpp
+++ samples/simple_out.cpp
@@ -56,10 +56,10 @@ int main()
 	signal(SIGINT, signalHandler);
 
 	// Pin Definitions
-	int output_pin = 18; // BOARD pin 12, BCM pin 18
+	int output_pin = 37; // BOARD pin 12, BCM pin 18
 
 	// Pin Setup.
-	GPIO::setmode(GPIO::BCM);
+	GPIO::setmode(GPIO::BOARD);
 	// set pin as an output pin with optional initial state of HIGH
 	GPIO::setup(output_pin, GPIO::OUT, GPIO::HIGH);
 
